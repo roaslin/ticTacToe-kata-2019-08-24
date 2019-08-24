40e89279530b31644263f7368349e544fd0bf426
roaslin <roaslin@gmail.com>
2019-08-24 11:25:24 +0200

    players are not allowed to place twice in a row

--- src/main/java/TicTactToe.java
+++ src/main/java/TicTactToe.java
@@ -1,7 +1,14 @@
 public class TicTactToe {
+    private String lastPlaced;
+
     public void place(String player) {
         if ("O".equalsIgnoreCase(player)) {
             throw new PlayerXGoesFirstException();
         }
+
+        if (player.equalsIgnoreCase(lastPlaced)) {
+            throw new AlternatePlayerException(); // message indicating which player should move
+        }
+        this.lastPlaced = player;
     }
 }
--- src/test/java/TicTacToeShould.java
+++ src/test/java/TicTacToeShould.java
@@ -1,19 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.assertNotNull;
 
 public class TicTacToeShould {
 
-    @Test
-    public void exist() {
+    private TicTactToe ticTacToe;
 
-        TicTactToe ticTacToe = new TicTactToe();
-        assertNotNull(ticTacToe);
+    @Before
+    public void setUp() {
+        ticTacToe = new TicTactToe();
     }
 
     @Test(expected = PlayerXGoesFirstException.class)
     public void does_not_allow_player_O_place_first() {
         TicTactToe ticTacToe = new TicTactToe();
+        ticTacToe.place("O");
+    }
+
+    @Test(expected = AlternatePlayerException.class)
+    public void not_let_player_place_twice_in_a_row() {
+        ticTacToe.place("X");
+        ticTacToe.place("X");
         ticTacToe.place("O");
     }
 }
