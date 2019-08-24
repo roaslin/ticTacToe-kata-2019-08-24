7fc28490c0519f967b3d508f7d71121f39ae7b8d
roaslin <roaslin@gmail.com>
2019-08-24 11:32:56 +0200

    refactor methods

--- src/main/java/TicTactToe.java
+++ src/main/java/TicTactToe.java
@@ -2,13 +2,21 @@ public class TicTactToe {
     private String lastPlaced;
 
     public void place(String player) {
-        if ("O".equalsIgnoreCase(player)) {
+        if (isPlayerOPlacingFirst(player)) {
             throw new PlayerXGoesFirstException();
         }
 
-        if (player.equalsIgnoreCase(lastPlaced)) {
+        if (isAlternatePlacingPlayer(player)) {
             throw new AlternatePlayerException(); // message indicating which player should move
         }
         this.lastPlaced = player;
+    }
+
+    private boolean isAlternatePlacingPlayer(String player) {
+        return player.equalsIgnoreCase(lastPlaced);
+    }
+
+    private boolean isPlayerOPlacingFirst(String player) {
+        return "O".equalsIgnoreCase(player) && lastPlaced == null;
     }
 }
